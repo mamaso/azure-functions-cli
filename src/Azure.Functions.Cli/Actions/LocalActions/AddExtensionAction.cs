e08c77a64de853b188fdeb1564f253e723879779
Matt Mason <mamaso@microsoft.com>
2017-09-28 23:52:37 -0700

    starting extensions

---
+++ src/Azure.Functions.Cli/Actions/LocalActions/AddExtensionAction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Functions.Cli.Interfaces;
+using Colors.Net;
+using Fclp;
+using System.Runtime.InteropServices;
+using static Azure.Functions.Cli.Common.OutputTheme;
+using Azure.Functions.Cli.Properties;
+
+namespace Azure.Functions.Cli.Actions.LocalActions
+{
+    [Action(Name = "add", Context = Context.Extensions, HelpText = "Create a new function from a template.")]
+    internal class AddExtensionAction : BaseAction
+    {
+        public string Package { get; set; }
+        public string Version { get; set; }
+
+        public AddExtensionAction()
+        {
+        }
+
+        public override ICommandLineParserResult ParseArgs(string[] args)
+        {
+            Parser
+                .Setup<string>('p', "package")
+                .WithDescription("Extension package")
+                .Callback(p => Package = p);
+            Parser
+                .Setup<string>('v', "version")
+                .WithDescription("Extension version")
+                .Callback(v => Version = v);
+
+            return Parser.Parse(args);
+        }
+
+        public async override Task RunAsync()
+        {
+            var proj = Resources.ExtensionsProj;
+        }
+    }
+}
